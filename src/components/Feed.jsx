9675ec21d50f57aa00961a94100b233da8362324
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-06-06 22:39:07 +0200

    to fix API 403 error

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import {Sidebar, Videos} from "./";
+
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New')
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+  },[selectedCategory])
   return (
-    <div>Feed</div>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: 'auto', md:'90.5vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
+        <Sidebar 
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff', marginRight: '15px' }}>Copyright 2023 Navas Media</Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflow: 'auto', height: '90vh', flex: 2 }}>
+        <Typography variant="h4" fontWeight='bold' mb={2} sx={{ color: '#D8FF00' }} >
+          New <span style={{ color: '#999797'}}>videos</span>
+        </Typography>
+
+        <Videos videos={[]}/>
+      </Box>
+    </Stack>
   )
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,33 @@
+import { Stack } from "@mui/material";
+
+import { categories } from '../utils/constans';
+
+const selectedCategory = "New";
+
+const Sidebar = () => (
+    <Stack
+      direction="row"
+      sx={{
+        overflow: "auto",
+        height: { sx: 'auto', md: '95%' },
+        flexDirection: { md: 'column' },
+      }}
+    >
+        {categories.map((category) => (
+            <button
+               className="category-btn"
+               style={{
+                background: category.name === selectedCategory && '#D8FF00',
+                color: '#999797',
+               }}
+               key={category.name}
+            >
+                <span style={{ color: category.name === selectedCategory ?  '#999797' : '#D8FF00'}}>{category.icon}</span>
+                <span style={{ opacity: categories.name === selectedCategory ? '1' : '0.8' }}>{category.name}</span>
+            </button>
+        ))}
+    </Stack>
+  )
+
+
+export default Sidebar
