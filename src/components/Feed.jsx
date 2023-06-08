f7afad6590c896482978428e170ef06a85e0825d
Gianfranco Navas Fernandini <gianf2605@gmail.com>
2023-06-08 13:33:17 +0200

    videocomponent OK

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,36 +1,50 @@
 import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import { fetchFromAPI } from "../utils/fetchFromAPI";
-import {Sidebar, Videos} from "./";
-
+import { Sidebar, Videos } from "./";
 
 const Feed = () => {
-
-  const [selectedCategory, setSelectedCategory] = useState('New')
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-  },[selectedCategory])
+    const fetchData = async () => {
+      try {
+        const response = await fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+        setVideos(response.items);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
-      <Box sx={{ height: { sx: 'auto', md:'90.5vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
-        <Sidebar 
+      <Box sx={{ height: { sx: 'auto', md: '90.5vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
+        <Sidebar
           selectedCategory={selectedCategory}
           setSelectedCategory={setSelectedCategory}
         />
 
-        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: '#fff', marginRight: '15px' }}>Copyright 2023 Navas Media</Typography>
+        <Typography
+          variant="body2"
+          sx={{ mt: 1.5, color: '#fff', marginRight: '15px' }}
+        >
+          Copyright 2023 Navas Media
+        </Typography>
       </Box>
 
       <Box p={2} sx={{ overflow: 'auto', height: '90vh', flex: 2 }}>
-        <Typography variant="h4" fontWeight='bold' mb={2} sx={{ color: '#D8FF00' }} >
-          New <span style={{ color: '#999797'}}>videos</span>
+        <Typography variant="h4" fontWeight='bold' mb={2} sx={{ color: '#D8FF00' }}>
+          New <span style={{ color: '#999797' }}>videos</span>
         </Typography>
 
-        <Videos videos={[]}/>
+        <Videos videos={videos} />
       </Box>
     </Stack>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,19 +1,24 @@
-import axios from "axios";
+
+
+const axios = require('axios');
 
 const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
+
 const options = {
+  url: BASE_URL,
+  params: {
+    part: 'id,snippet',
+    type: 'video',
+    maxResults: '50'
+  },
+  headers: {
+    'X-RapidAPI-Key': '365eb4d1acmshd84bf04c60250b5p1796c3jsn9f75262fdba3',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+  }
+};
 
-    url: BASE_URL,
-    params: {
-      maxResults: '50'
-    },
-    headers: {
-      'X-RapidAPI-Key': process.env.PROCESS_APP_RAPID_API_KEY,
-      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
-    }
-  };
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
-  export const fetchFromAPI = async (url) => {
-       const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-       return data;
-  } 
+  return data;
+};
